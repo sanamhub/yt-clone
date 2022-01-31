a469582bd0266817a3324c828ebefc96eb46bd4a
Sanam <sanampakuwal27@gmail.com>
2022-01-31 22:13:36 +0545

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# yt-clone
+
+## PR are always welcome 🚀
+
+- Please create new issue if you noticed
+
+- Comment on the issue if you want to work on it
---
+++ script.js
@@ -0,0 +1,25 @@
+// todo
+// Implementation of YouTube Data API V3
+// generate videos as per search
+
+// sample video element you can dynamically generate via javascript
+/*
+<div class="video">
+  <div class="thumbnail">
+    <img
+      src="https://www.91-cdn.com/hub/wp-content/uploads/2020/03/YouTube.jpg"
+      alt=""
+    />
+  </div>
+  <div class="details">
+    <div class="author">
+      <img src="https://www.sanampakuwal.com.np/favicon.ico" alt="" />
+    </div>
+    <div class="title">
+      <h3>Understanding beyond the human intelligence | Explore Unexplored</h3>
+      <a href="">Sanam Pakuwal</a>
+      <span>2M views • 3 months ago</span>
+    </div>
+  </div>
+</div>
+*/
